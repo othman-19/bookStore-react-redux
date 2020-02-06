f399472a5977f6a2fa12a77f2e47d6680c0e7564
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-06 15:16:48 +0530

    set up Appcomponent and other Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import BooksList from './components/BooksList';
+import BooksForm from './components/BooksForm';
 
 function App() {
   return (
     <div className="App">
       <h1> Book Store Project...</h1>
+      <BooksList />
+      <BooksForm />
     </div>
   );
 }
