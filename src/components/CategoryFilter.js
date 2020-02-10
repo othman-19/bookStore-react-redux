3b5c1fa97acf583840b99980b89f440550560748
Sumaer Jolly <sumaerjolly@gmail.com>
2020-02-10 21:22:42 +0530

    filter added to the right simply

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -14,8 +14,8 @@ function CategoryFilter({ filterChangeHandler }) {
   ];
 
   return (
-    <div>
-      <h3>Select Book Category:</h3>
+    <div className="filter">
+      <span className="filter-select">Select Book Category:</span>
       <select name="filter" onChange={e => filterChangeHandler(e.target.value)}>
         {filters.map(filter => (
           <option key={filter}>{filter}</option>
